e380a2fc083ca4865c96a52f9f35c0818fbac453
Tommy Lau <tommy.lhg@gmail.com>
2022-04-08 03:23:14 +0800

    Generate site license with expiration date on Dec 31, 2099

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,16 +7,16 @@ int main() {
     std::cout << "Input your name(English Only): ";
     std::getline(std::cin, name);
 
-    std::tm ZeroDate = { 0, 0, 0, 1, 1, 1970 - 1900 };
+    std::tm ZeroDate = { 0, 0, 0, 1, 1, 1970 };
 
-    int ExpireYear = 2020;
-    int ExpireMonth = 6;
-    int ExpireDay = 23;
-    std::tm ExpireDate = { 0, 0, 0, ExpireDay, ExpireMonth, ExpireYear - 1900 };
+    int ExpireYear = 2099;
+    int ExpireMonth = 12;
+    int ExpireDay = 31;
+    std::tm ExpireDate = { 0, 0, 0, ExpireDay, ExpireMonth, ExpireYear };
     uint32_t ExpireDaystamp = static_cast<uint32_t>(std::difftime(std::mktime(&ExpireDate), std::mktime(&ZeroDate)) / 3600 / 24);
 
     std::vector<byte> name_bytes(name.begin(), name.end());
     _010Editor::Keygen<_010Editor::KeyType::TimeLicense> keygen;
-    std::cout << keygen.GetKey(name_bytes, ExpireDaystamp, 1) << std::endl;
+    std::cout << keygen.GetKey(name_bytes, ExpireDaystamp, 1000) << std::endl;
     return 0;
 }
